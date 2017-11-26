f9df3a574a117e6efaeef175edd75ac125352b00
svetluy <mikitn2009@yandex.ru>
2017-11-26 18:24:06 +0400

    как же я заебался, но сделал

---
+++ Labs/Lab4/Form1.Designer.cs
@@ -0,0 +1,159 @@
+﻿namespace Lab4
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.файлToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.открытьToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.сохранитьToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.Year = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Country = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Winner = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.PrizeWinner = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.количествоПобедToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AllowUserToDeleteRows = false;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Year,
+            this.Country,
+            this.Winner,
+            this.PrizeWinner});
+            this.dataGridView1.Location = new System.Drawing.Point(-2, 24);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(503, 310);
+            this.dataGridView1.TabIndex = 0;
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.файлToolStripMenuItem,
+            this.количествоПобедToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(512, 24);
+            this.menuStrip1.TabIndex = 1;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // файлToolStripMenuItem
+            // 
+            this.файлToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.открытьToolStripMenuItem,
+            this.сохранитьToolStripMenuItem});
+            this.файлToolStripMenuItem.Name = "файлToolStripMenuItem";
+            this.файлToolStripMenuItem.Size = new System.Drawing.Size(48, 20);
+            this.файлToolStripMenuItem.Text = "Файл";
+            // 
+            // открытьToolStripMenuItem
+            // 
+            this.открытьToolStripMenuItem.Name = "открытьToolStripMenuItem";
+            this.открытьToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.открытьToolStripMenuItem.Text = "Открыть";
+            this.открытьToolStripMenuItem.Click += new System.EventHandler(this.открытьToolStripMenuItem_Click);
+            // 
+            // сохранитьToolStripMenuItem
+            // 
+            this.сохранитьToolStripMenuItem.Name = "сохранитьToolStripMenuItem";
+            this.сохранитьToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.сохранитьToolStripMenuItem.Text = "Сохранить";
+            this.сохранитьToolStripMenuItem.Click += new System.EventHandler(this.сохранитьToolStripMenuItem_Click);
+            // 
+            // Year
+            // 
+            this.Year.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.None;
+            this.Year.HeaderText = "Год Чемпионата";
+            this.Year.Name = "Year";
+            // 
+            // Country
+            // 
+            this.Country.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.None;
+            this.Country.HeaderText = "Страна-хозяйка турнира";
+            this.Country.Name = "Country";
+            // 
+            // Winner
+            // 
+            this.Winner.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.None;
+            this.Winner.HeaderText = "Победитель";
+            this.Winner.Name = "Winner";
+            this.Winner.ReadOnly = true;
+            // 
+            // PrizeWinner
+            // 
+            this.PrizeWinner.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.DisplayedCells;
+            this.PrizeWinner.HeaderText = "Призеры";
+            this.PrizeWinner.Name = "PrizeWinner";
+            this.PrizeWinner.ReadOnly = true;
+            this.PrizeWinner.Width = 78;
+            // 
+            // количествоПобедToolStripMenuItem
+            // 
+            this.количествоПобедToolStripMenuItem.Name = "количествоПобедToolStripMenuItem";
+            this.количествоПобедToolStripMenuItem.Size = new System.Drawing.Size(120, 20);
+            this.количествоПобедToolStripMenuItem.Text = "Количество побед";
+            this.количествоПобедToolStripMenuItem.Click += new System.EventHandler(this.количествоПобедToolStripMenuItem_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(512, 367);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem файлToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem открытьToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem сохранитьToolStripMenuItem;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Year;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Country;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Winner;
+        private System.Windows.Forms.DataGridViewTextBoxColumn PrizeWinner;
+        private System.Windows.Forms.ToolStripMenuItem количествоПобедToolStripMenuItem;
+    }
+}
+
---
+++ Labs/Lab4/Form1.cs
@@ -0,0 +1,149 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Lab4
+{
+    public partial class Form1 : Form
+    {
+        int linesCount;
+        Dictionary<string,int> winners= new Dictionary<string,int>();
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void открытьToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog OPF = new OpenFileDialog();
+            OPF.Filter = "txt files (*.txt)|*.txt";
+            if (OPF.ShowDialog() == DialogResult.OK)
+            {
+                string[] lines = null;
+                dataGridView1.Rows.Clear();
+                try
+                {
+                    lines = File.ReadAllLines(OPF.FileName, Encoding.Unicode);
+                    linesCount = lines.Length;
+                }
+                catch (Exception e3)
+                {
+                    linesCount = 0;
+                    MessageBox.Show(e3.Message);
+                }
+
+                FillTable(lines);
+            }
+
+        }
+
+        private void сохранитьToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            SaveFileDialog SVF = new SaveFileDialog();
+            SVF.Filter = "txt files (*.txt)|*.txt";
+            if (SVF.ShowDialog() == DialogResult.OK)
+            {
+                string line = "";
+                try
+                {
+                    using (StreamWriter sw = new StreamWriter(SVF.FileName, true, Encoding.Unicode))
+                        for (int i = 0; i < linesCount; i++)
+                            if (dataGridView1.Rows[i].Visible)
+                            {
+                                for (int j = 0; j < 4; j++)
+                                    try
+                                    {
+                                        line += (dataGridView1.Rows[i].Cells[j].Value);
+                                        if (j!=3)
+                                            line += ":";
+                                    }
+                                    catch (Exception e1)
+                                    {
+                                        MessageBox.Show(e1.Message);
+                                    }
+                                sw.WriteLine(line);
+                                line = "";
+                            }
+                }
+                catch (Exception e3)
+                {
+                    MessageBox.Show(e3.Message);
+                }
+
+            }
+        }
+
+        private void FillTable(string[] lines)
+        {
+            int count = 0;
+            int sum = 0;
+
+            for (int i = 0; i < linesCount; i++) // Цикл добавления строк
+                dataGridView1.Rows.Add();  // добавление строки 
+
+            for (int i = 0; i < linesCount; i++)
+            {
+                for (int j = 0; j < 4; j++)
+                    try
+                    {
+                        String[] substrings = lines[sum].Split(':');
+                        dataGridView1.Rows[i].Cells[j].Value = substrings[j];
+                        if(j==2)
+                        {
+                            if (winners.ContainsKey(substrings[j].Substring(0,3)))
+                            {
+                                winners[substrings[j].Substring(0, 3)]++;
+                            }
+                            else
+                            winners.Add(substrings[j].Substring(0, 3), 1);
+                        }
+                    }
+                    catch (Exception e1)
+                    {
+                          MessageBox.Show(e1.Message);
+                    }
+               // count = 0;
+                sum++;
+            }
+        }
+
+        private void количествоПобедToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+           
+            dataGridView1.Rows.Clear();
+            dataGridView1.Columns.Clear();
+            dataGridView1.Columns.Add("Winners","Победители");
+            dataGridView1.Columns.Add("Count", "Количество побед");
+
+            for (int i = 0; i < linesCount; i++) // Цикл добавления строк
+                dataGridView1.Rows.Add();  // добавление строки 
+
+
+
+            int j = 0;
+
+            try
+            {
+                foreach (var win in winners)
+                {
+                    dataGridView1.Rows[j].Cells[0].Value = win.Key;
+                    dataGridView1.Rows[j].Cells[1].Value = win.Value;
+                    j++;
+                }
+            }
+            catch (Exception e1)
+            {
+                MessageBox.Show(e1.Message);
+            }
+            
+        }
+    }
+}
